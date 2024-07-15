9e67f33ec261eeaf0cdc0a123879315af30b3a76
raosahab14127 <eb20103109.raohammadali@gmail.com>
2024-07-15 09:03:20 -0400

    input comp made

---
+++ components/input.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {Input} from 'react-native-elements';
+
+function Primaryinput(props) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>{props.title}</Text>
+      <Input
+        inputContainerStyle={styles.inputContainer}
+        inputStyle={styles.input}
+        secureTextEntry={props.secure}
+        placeholder={props.placeholder}
+        inputMode={props.type}
+        underlineColorAndroid="transparent"
+      />
+    </View>
+  );
+}
+
+export default Primaryinput;
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    borderRadius: 15,
+    backgroundColor: '#292929',
+  },
+  text: {
+    fontSize: 18,
+    marginLeft: 15,
+    color: '#FFE',
+    paddingTop: 5,
+  },
+  inputContainer: {
+    borderBottomWidth: 0,
+    paddingBottom: '0',
+  },
+  input: {
+    color: '#FFE',
+    fontSize: 16,
+    marginBottom: -30,
+  },
+});
